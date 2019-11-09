ef7ffe0358620e67aee0761dafad1eab162c7b3f
Felix Schuergut <schuergf@student.hu-berlin.de>
2019-11-09 04:46:59 +0100

    added settings for the addon

--- addon.py
+++ addon.py
@@ -5,9 +5,25 @@ import xbmc
 import xbmcaddon
 import Adafruit_DHT
 
+__addon__ = xbmcaddon.Addon()
+__addonname__ = __addon__.getAddonInfo('name')
+__icon__ = __addon__.getAddonInfo('icon')
+__settings__ = xbmcaddon.Addon(id='script.inttemp')
+__language__ = __settings__.getLocalizedString
+
+
 # Sensor should be set to Adafruit_DHT.DHT11,
 # Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
-sensor = Adafruit_DHT.AM2302
+
+sensor_args = [
+     Adafruit_DHT.DHT11,
+     Adafruit_DHT.DHT22,
+     Adafruit_DHT.AM2302
+]
+
+sensor = sensor_args[int(__settings__.getSetting('model'), base=10)]
+pin = int(__settings__.getSetting('pin'), base=10)
+time = int(__settings__.getSetting('time')) * 1000
 
 # Example using a Beaglebone Black with DHT sensor
 # connected to pin P8_11.
@@ -15,7 +31,7 @@ sensor = Adafruit_DHT.AM2302
 
 # Example using a Raspberry Pi with DHT sensor
 # connected to GPIO24.
-pin = 24
+#pin = 24
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
@@ -26,18 +42,13 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
 
-__addon__ = xbmcaddon.Addon()
-__addonname__ = __addon__.getAddonInfo('name')
-__icon__ = __addon__.getAddonInfo('icon')
-
 if humidity is not None and temperature is not None:
-    line1 = 'Temp={0:0.1f}°C  Humidity={1:0.1f}%rLF'.format(temperature, humidity)
-
+    line1 = 'Temp: {0:0.1f}°C '.format(temperature) + __settings__.getLocalizedString(32014) + ': {0:0.1f}%rLF'.format(humidity)
 
 else:
-    line1 = 'Failed to get reading. Try again!'
+    line1 = __settings__.getLocalizedString(32015)
 
 #line1 = "This is a simple example of notifications"
-time = 8000 #in miliseconds
+#time = 8000 #in miliseconds
 
 xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(__addonname__,line1, time, __icon__))
